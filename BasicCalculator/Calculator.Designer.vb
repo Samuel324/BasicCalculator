fcfb9d8801b2dd870fff243363f00697440e0967
Samuel <thebronzeaxe3@gmail.com>
2021-02-15 15:39:56 +1100

    Add project files.

---
+++ BasicCalculator/Calculator.Designer.vb
@@ -0,0 +1,345 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Calculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpClear = New System.Windows.Forms.Button()
+        Me.Num0 = New System.Windows.Forms.Button()
+        Me.NumDot = New System.Windows.Forms.Button()
+        Me.OpEquals = New System.Windows.Forms.Button()
+        Me.OpPlus = New System.Windows.Forms.Button()
+        Me.Num3 = New System.Windows.Forms.Button()
+        Me.Num2 = New System.Windows.Forms.Button()
+        Me.Num1 = New System.Windows.Forms.Button()
+        Me.OpMinus = New System.Windows.Forms.Button()
+        Me.Num6 = New System.Windows.Forms.Button()
+        Me.Num5 = New System.Windows.Forms.Button()
+        Me.Num4 = New System.Windows.Forms.Button()
+        Me.OpMultiply = New System.Windows.Forms.Button()
+        Me.Num9 = New System.Windows.Forms.Button()
+        Me.Num8 = New System.Windows.Forms.Button()
+        Me.Num7 = New System.Windows.Forms.Button()
+        Me.OpDivide = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'OpClear
+        '
+        Me.OpClear.BackColor = System.Drawing.Color.Black
+        Me.OpClear.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.OpClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.OpClear.ForeColor = System.Drawing.Color.White
+        Me.OpClear.Location = New System.Drawing.Point(3, 251)
+        Me.OpClear.Name = "OpClear"
+        Me.OpClear.Size = New System.Drawing.Size(68, 49)
+        Me.OpClear.TabIndex = 1
+        Me.OpClear.Text = "Clr"
+        Me.OpClear.UseVisualStyleBackColor = False
+        '
+        'Num0
+        '
+        Me.Num0.BackColor = System.Drawing.Color.Black
+        Me.Num0.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Num0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num0.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Num0.ForeColor = System.Drawing.Color.White
+        Me.Num0.Location = New System.Drawing.Point(71, 251)
+        Me.Num0.Name = "Num0"
+        Me.Num0.Size = New System.Drawing.Size(68, 49)
+        Me.Num0.TabIndex = 2
+        Me.Num0.Text = "0"
+        Me.Num0.UseVisualStyleBackColor = False
+        '
+        'NumDot
+        '
+        Me.NumDot.BackColor = System.Drawing.Color.Black
+        Me.NumDot.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.NumDot.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.NumDot.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.NumDot.ForeColor = System.Drawing.Color.White
+        Me.NumDot.Location = New System.Drawing.Point(139, 251)
+        Me.NumDot.Name = "NumDot"
+        Me.NumDot.Size = New System.Drawing.Size(68, 49)
+        Me.NumDot.TabIndex = 3
+        Me.NumDot.Text = "."
+        Me.NumDot.UseVisualStyleBackColor = False
+        '
+        'OpEquals
+        '
+        Me.OpEquals.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpEquals.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpEquals.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpEquals.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.OpEquals.ForeColor = System.Drawing.Color.White
+        Me.OpEquals.Location = New System.Drawing.Point(206, 251)
+        Me.OpEquals.Name = "OpEquals"
+        Me.OpEquals.Size = New System.Drawing.Size(68, 49)
+        Me.OpEquals.TabIndex = 4
+        Me.OpEquals.Text = "="
+        Me.OpEquals.UseVisualStyleBackColor = False
+        '
+        'OpPlus
+        '
+        Me.OpPlus.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpPlus.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpPlus.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpPlus.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.OpPlus.ForeColor = System.Drawing.Color.White
+        Me.OpPlus.Location = New System.Drawing.Point(206, 202)
+        Me.OpPlus.Name = "OpPlus"
+        Me.OpPlus.Size = New System.Drawing.Size(68, 49)
+        Me.OpPlus.TabIndex = 8
+        Me.OpPlus.Text = "+"
+        Me.OpPlus.UseVisualStyleBackColor = False
+        '
+        'Num3
+        '
+        Me.Num3.BackColor = System.Drawing.Color.Black
+        Me.Num3.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Num3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Num3.ForeColor = System.Drawing.Color.White
+        Me.Num3.Location = New System.Drawing.Point(139, 202)
+        Me.Num3.Name = "Num3"
+        Me.Num3.Size = New System.Drawing.Size(68, 49)
+        Me.Num3.TabIndex = 7
+        Me.Num3.Text = "3"
+        Me.Num3.UseVisualStyleBackColor = False
+        '
+        'Num2
+        '
+        Me.Num2.BackColor = System.Drawing.Color.Black
+        Me.Num2.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Num2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Num2.ForeColor = System.Drawing.Color.White
+        Me.Num2.Location = New System.Drawing.Point(71, 202)
+        Me.Num2.Name = "Num2"
+        Me.Num2.Size = New System.Drawing.Size(68, 49)
+        Me.Num2.TabIndex = 6
+        Me.Num2.Text = "2"
+        Me.Num2.UseVisualStyleBackColor = False
+        '
+        'Num1
+        '
+        Me.Num1.BackColor = System.Drawing.Color.Black
+        Me.Num1.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Num1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Num1.ForeColor = System.Drawing.Color.White
+        Me.Num1.Location = New System.Drawing.Point(3, 202)
+        Me.Num1.Name = "Num1"
+        Me.Num1.Size = New System.Drawing.Size(68, 49)
+        Me.Num1.TabIndex = 5
+        Me.Num1.Text = "1"
+        Me.Num1.UseVisualStyleBackColor = False
+        '
+        'OpMinus
+        '
+        Me.OpMinus.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpMinus.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpMinus.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpMinus.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.OpMinus.ForeColor = System.Drawing.Color.White
+        Me.OpMinus.Location = New System.Drawing.Point(206, 153)
+        Me.OpMinus.Name = "OpMinus"
+        Me.OpMinus.Size = New System.Drawing.Size(68, 49)
+        Me.OpMinus.TabIndex = 12
+        Me.OpMinus.Text = "-"
+        Me.OpMinus.UseVisualStyleBackColor = False
+        '
+        'Num6
+        '
+        Me.Num6.BackColor = System.Drawing.Color.Black
+        Me.Num6.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Num6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Num6.ForeColor = System.Drawing.Color.White
+        Me.Num6.Location = New System.Drawing.Point(139, 153)
+        Me.Num6.Name = "Num6"
+        Me.Num6.Size = New System.Drawing.Size(68, 49)
+        Me.Num6.TabIndex = 11
+        Me.Num6.Text = "6"
+        Me.Num6.UseVisualStyleBackColor = False
+        '
+        'Num5
+        '
+        Me.Num5.BackColor = System.Drawing.Color.Black
+        Me.Num5.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Num5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Num5.ForeColor = System.Drawing.Color.White
+        Me.Num5.Location = New System.Drawing.Point(71, 153)
+        Me.Num5.Name = "Num5"
+        Me.Num5.Size = New System.Drawing.Size(68, 49)
+        Me.Num5.TabIndex = 10
+        Me.Num5.Text = "5"
+        Me.Num5.UseVisualStyleBackColor = False
+        '
+        'Num4
+        '
+        Me.Num4.BackColor = System.Drawing.Color.Black
+        Me.Num4.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Num4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Num4.ForeColor = System.Drawing.Color.White
+        Me.Num4.Location = New System.Drawing.Point(3, 153)
+        Me.Num4.Name = "Num4"
+        Me.Num4.Size = New System.Drawing.Size(68, 49)
+        Me.Num4.TabIndex = 9
+        Me.Num4.Text = "4"
+        Me.Num4.UseVisualStyleBackColor = False
+        '
+        'OpMultiply
+        '
+        Me.OpMultiply.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpMultiply.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpMultiply.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpMultiply.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.OpMultiply.ForeColor = System.Drawing.Color.White
+        Me.OpMultiply.Location = New System.Drawing.Point(206, 104)
+        Me.OpMultiply.Name = "OpMultiply"
+        Me.OpMultiply.Size = New System.Drawing.Size(68, 49)
+        Me.OpMultiply.TabIndex = 16
+        Me.OpMultiply.Text = "x"
+        Me.OpMultiply.UseVisualStyleBackColor = False
+        '
+        'Num9
+        '
+        Me.Num9.BackColor = System.Drawing.Color.Black
+        Me.Num9.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Num9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num9.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Num9.ForeColor = System.Drawing.Color.White
+        Me.Num9.Location = New System.Drawing.Point(139, 104)
+        Me.Num9.Name = "Num9"
+        Me.Num9.Size = New System.Drawing.Size(68, 49)
+        Me.Num9.TabIndex = 15
+        Me.Num9.Text = "9"
+        Me.Num9.UseVisualStyleBackColor = False
+        '
+        'Num8
+        '
+        Me.Num8.BackColor = System.Drawing.Color.Black
+        Me.Num8.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Num8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Num8.ForeColor = System.Drawing.Color.White
+        Me.Num8.Location = New System.Drawing.Point(71, 104)
+        Me.Num8.Name = "Num8"
+        Me.Num8.Size = New System.Drawing.Size(68, 49)
+        Me.Num8.TabIndex = 14
+        Me.Num8.Text = "8"
+        Me.Num8.UseVisualStyleBackColor = False
+        '
+        'Num7
+        '
+        Me.Num7.BackColor = System.Drawing.Color.Black
+        Me.Num7.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Num7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Num7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Num7.ForeColor = System.Drawing.Color.White
+        Me.Num7.Location = New System.Drawing.Point(3, 104)
+        Me.Num7.Name = "Num7"
+        Me.Num7.Size = New System.Drawing.Size(68, 49)
+        Me.Num7.TabIndex = 13
+        Me.Num7.Text = "7"
+        Me.Num7.UseVisualStyleBackColor = False
+        '
+        'OpDivide
+        '
+        Me.OpDivide.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpDivide.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpDivide.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.OpDivide.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.OpDivide.ForeColor = System.Drawing.Color.White
+        Me.OpDivide.Location = New System.Drawing.Point(206, 55)
+        Me.OpDivide.Name = "OpDivide"
+        Me.OpDivide.Size = New System.Drawing.Size(68, 49)
+        Me.OpDivide.TabIndex = 20
+        Me.OpDivide.Text = "÷"
+        Me.OpDivide.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox1.Location = New System.Drawing.Point(3, 9)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(0)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(271, 28)
+        Me.TextBox1.TabIndex = 25
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Calculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(277, 319)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.OpDivide)
+        Me.Controls.Add(Me.OpMultiply)
+        Me.Controls.Add(Me.Num9)
+        Me.Controls.Add(Me.Num8)
+        Me.Controls.Add(Me.Num7)
+        Me.Controls.Add(Me.OpMinus)
+        Me.Controls.Add(Me.Num6)
+        Me.Controls.Add(Me.Num5)
+        Me.Controls.Add(Me.Num4)
+        Me.Controls.Add(Me.OpPlus)
+        Me.Controls.Add(Me.Num3)
+        Me.Controls.Add(Me.Num2)
+        Me.Controls.Add(Me.Num1)
+        Me.Controls.Add(Me.OpEquals)
+        Me.Controls.Add(Me.NumDot)
+        Me.Controls.Add(Me.Num0)
+        Me.Controls.Add(Me.OpClear)
+        Me.Name = "Calculator"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents OpClear As Button
+    Friend WithEvents Num0 As Button
+    Friend WithEvents NumDot As Button
+    Friend WithEvents OpEquals As Button
+    Friend WithEvents OpPlus As Button
+    Friend WithEvents Num3 As Button
+    Friend WithEvents Num2 As Button
+    Friend WithEvents Num1 As Button
+    Friend WithEvents OpMinus As Button
+    Friend WithEvents Num6 As Button
+    Friend WithEvents Num5 As Button
+    Friend WithEvents Num4 As Button
+    Friend WithEvents OpMultiply As Button
+    Friend WithEvents Num9 As Button
+    Friend WithEvents Num8 As Button
+    Friend WithEvents Num7 As Button
+    Friend WithEvents OpDivide As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ BasicCalculator/Calculator.vb
@@ -0,0 +1,171 @@
+﻿Public Class Calculator
+
+    Private Sub Num1_Click(sender As Object, e As EventArgs) Handles Num1.Click
+
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "Num1" Then
+            TextBox1.Text = TextBox1.Text + "1"
+
+        End If
+    End Sub
+
+    Private Sub Num2_Click(sender As Object, e As EventArgs) Handles Num2.Click
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "Num2" Then
+            TextBox1.Text = TextBox1.Text + "2"
+
+        End If
+    End Sub
+
+    Private Sub Num3_Click(sender As Object, e As EventArgs) Handles Num3.Click
+
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "Num3" Then
+            TextBox1.Text = TextBox1.Text + "3"
+
+        End If
+    End Sub
+
+    Private Sub Num4_Click(sender As Object, e As EventArgs) Handles Num4.Click
+
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "Num4" Then
+            TextBox1.Text = TextBox1.Text + "4"
+
+        End If
+    End Sub
+
+    Private Sub Num5_Click(sender As Object, e As EventArgs) Handles Num5.Click
+
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "Num5" Then
+            TextBox1.Text = TextBox1.Text + "5"
+
+        End If
+    End Sub
+
+    Private Sub Num6_Click(sender As Object, e As EventArgs) Handles Num6.Click
+
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "Num6" Then
+            TextBox1.Text = TextBox1.Text + "6"
+
+        End If
+
+    End Sub
+
+    Private Sub Num7_Click(sender As Object, e As EventArgs) Handles Num7.Click
+
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "Num7" Then
+            TextBox1.Text = TextBox1.Text + "7"
+
+        End If
+
+    End Sub
+
+    Private Sub Num8_Click(sender As Object, e As EventArgs) Handles Num8.Click
+
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "Num8" Then
+            TextBox1.Text = TextBox1.Text + "8"
+
+        End If
+
+    End Sub
+
+    Private Sub Num9_Click(sender As Object, e As EventArgs) Handles Num9.Click
+
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "Num9" Then
+            TextBox1.Text = TextBox1.Text + "9"
+
+        End If
+
+    End Sub
+
+    Private Sub NumDot_Click(sender As Object, e As EventArgs) Handles NumDot.Click
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "NumDot" Then
+            TextBox1.Text = TextBox1.Text + "."
+
+        End If
+
+    End Sub
+
+    Private Sub Num0_Click(sender As Object, e As EventArgs) Handles Num0.Click
+
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "Num0" Then
+            TextBox1.Text = TextBox1.Text + "0"
+
+        End If
+    End Sub
+
+    Private Sub OpEquals_Click(sender As Object, e As EventArgs) Handles OpEquals.Click
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "OpEquals" Then
+            Dim equation As String = TextBox1.Text
+            Dim result = New DataTable().Compute(equation, Nothing)
+            TextBox1.Text = result
+
+        End If
+    End Sub
+
+    Private Sub OpPlus_Click(sender As Object, e As EventArgs) Handles OpPlus.Click
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "OpPlus" Then
+            TextBox1.Text = TextBox1.Text + "+"
+
+        End If
+    End Sub
+
+    Private Sub OpMinus_Click(sender As Object, e As EventArgs) Handles OpMinus.Click
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "OpMinus" Then
+            TextBox1.Text = TextBox1.Text + "-"
+
+        End If
+    End Sub
+
+    Private Sub OpMultiply_Click(sender As Object, e As EventArgs) Handles OpMultiply.Click
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "OpMultiply" Then
+            TextBox1.Text = TextBox1.Text + "*"
+
+        End If
+    End Sub
+
+    Private Sub OpDivide_Click(sender As Object, e As EventArgs) Handles OpDivide.Click
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "OpDivide" Then
+            TextBox1.Text = TextBox1.Text + "/"
+
+        End If
+    End Sub
+
+    Private Sub OpClear_Click(sender As Object, e As EventArgs) Handles OpClear.Click
+        Dim button As Button = CType(sender, Button)
+
+        If button.Name = "OpClear" Then
+            TextBox1.Text = ""
+
+        End If
+    End Sub
+End Class
